d3d0cd716a8a34d71122e6aacef2e1dd3a4ad176
yw <3215790191@qq.com>
2019-07-26 11:30:25 +0800

    插入排序

---
+++ insertSort/sort.go
@@ -0,0 +1,25 @@
+package sort
+
+func insertSort(sli []int) []int {
+	length := len(sli)
+
+	if length <= 0 {
+		return sli
+	}
+
+	for i := 0; i < length; i++ {
+		tmp := sli[i]
+		//内层循环控制，比较并插入
+		for j := i - 1; j >= 0; j-- {
+
+			if sli[j] > tmp {
+				//发现插入的元素要小，交换位置，将后边的元素与前面的元素互换
+				sli[j+1], sli[j] = sli[j], tmp
+			} else {
+				//如果碰到不需要移动的元素，则前面的就不需要再次比较了。
+				break
+			}
+		}
+	}
+	return sli
+}
---
+++ insertSort/sort_test.go
@@ -0,0 +1,10 @@
+package sort
+
+import (
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	li := []int{1, 5, 32, 2, 3, 23, 12, 4, 22}
+	t.Log(insertSort(li))
+}
